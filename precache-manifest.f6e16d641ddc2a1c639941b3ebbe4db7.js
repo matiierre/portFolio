52dd62f4ba0103b54cca5f2414177182cb8a9427
Matias <matias_rodriguez_t@hotmail.com>
2020-05-15 14:08:21 +0200

    Updates

---
+++ precache-manifest.f6e16d641ddc2a1c639941b3ebbe4db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61713a80ddee8ada7e11ebac7fa9cc4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de8443f32edd693fdcc9",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "5a007fd528b318f69fab",
+    "url": "/static/css/main.ec0863ce.chunk.css"
+  },
+  {
+    "revision": "de8443f32edd693fdcc9",
+    "url": "/static/js/2.6d272ed3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.6d272ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a007fd528b318f69fab",
+    "url": "/static/js/main.f6477f8d.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
